29f0eefb28dcbecd8451c75beac62ee1bb554257
brianrscode <135724962+brianrscode@users.noreply.github.com>
2024-10-21 15:51:27 -0600

    Nueva forma

--- nuevo.py
+++ nuevo.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import networkx as nx
 import matplotlib.pyplot as plt
 import itertools as it
+import re
 
 
 #######################################################
@@ -10,17 +11,22 @@ import itertools as it
 #######################################################
 class AutomataFinitoDeterminista:
     def __init__(self):
+
         self.grafo = nx.MultiDiGraph()  # Grafo con múltiples aristas
 
     def agregar_estado(self, estado, tipo=None, color='skyblue'):
         '''Agrega un estado (nodo) al autómata.'''
-        self.grafo.add_node(estado)
         if tipo == 'final':
-            nx.set_node_attributes(self.grafo, {estado: 'red'}, 'color')
+            color = 'green'
         elif tipo == 'inicial':
-            nx.set_node_attributes(self.grafo, {estado: 'green'}, 'color')
-        else:
-            nx.set_node_attributes(self.grafo, {estado: color}, 'color')
+            color = 'yellow'
+        elif tipo == 'noAceptado':
+            color = 'red'
+        if not color:
+            color = 'skyblue'
+
+        self.grafo.add_node(estado)
+        nx.set_node_attributes(self.grafo, {estado: color}, 'color')
 
     def agregar_transicion(self, estado_origen, estado_destino, simbolo):
         '''Agrega una transición (arista) entre estados.'''
@@ -29,7 +35,7 @@ class AutomataFinitoDeterminista:
     def mostrar_automata(self):
         '''Dibuja el autómata usando NetworkX y Matplotlib.'''
         # Posición de los nodos
-        pos_nodos = nx.spring_layout(self.grafo, seed=5)
+        pos_nodos = nx.spring_layout(self.grafo, seed=5, k=1)
         # Colores de los nodos
         colores = [nx.get_node_attributes(self.grafo, 'color').get(n) for n in self.grafo.nodes()]
         # Etiquetas de los nodos
@@ -53,11 +59,80 @@ class AutomataFinitoDeterminista:
             pos_nodos,
             labels,
             connectionstyle=[f"arc3,rad={r}" for r in it.accumulate([0.15] * 4)],
-            label_pos=0.3,
+            label_pos=0.5,
             font_color="black",
-            bbox={"alpha": 0},
+            # bbox={"alpha": 0},  # Elimina el fondo del label
         )
         plt.show()
+
+    def q0(self, llave):
+        pat = r'[a-z]{5}'
+        self.agregar_estado("q0", tipo="inicial")
+        if not re.match(pat, llave):
+            self.agregar_estado("q8", tipo="noAceptado")
+            self.agregar_transicion("q0", "q8", f"{llave}")
+            return False
+        return True
+
+    def q1(self, texto_plano, llave):
+        self.agregar_estado("q1")
+        self.agregar_transicion("q0", "q1", f"{llave}")
+        if not re.match(r'[a-zA-Z0-9]{5,10}', texto_plano):
+            self.agregar_estado("q9", tipo="noAceptado")
+            self.agregar_transicion("q1", "q9", f"{llave}")
+            return False
+        return True
+
+    def q2(self, llave, texto_plano ):
+        llave_ascii = [ord(c) for c in llave]
+        self.agregar_estado("q2")
+        self.agregar_transicion("q1", "q2", f"{texto_plano}")
+        return llave_ascii
+
+    def q3(self, llave_ascii):
+        '''Algoritmo de KSA.'''
+        S = list(range(256))  # Tabla S
+        key_length = len(llave_ascii)
+        j = 0
+        for i in range(256):
+            j = (j + llave_ascii[i % key_length] + S[i]) % 256
+            S[i], S[j] = S[j], S[i]
+        self.agregar_estado("q3")
+        self.agregar_transicion("q2", "q3", f"{llave_ascii}")
+        return S
+
+    def q4(self, S, texto_plano):
+        i = 0
+        j = 0
+        k = 0
+        llaves = []
+        self.agregar_estado("q4")
+        self.agregar_transicion("q3", "q4", "KSA")
+        while k < len(texto_plano):
+            i = (i + 1) % 256
+            j = (j + S[i]) % 256
+            S[i], S[j] = S[j], S[i]
+            llaves.append(S[(S[i] + S[j]) % 256])
+            k += 1
+
+        return llaves
+
+    def q5(self, llaves, texto_plano):
+        self.agregar_estado("q5")
+        self.agregar_estado("q6")
+        self.agregar_transicion("q4", "q5", "PRGA")
+
+        texto_cifrado = ""
+        for idx, char in enumerate(texto_plano):
+            texto_cifrado += "%02X" % (ord(char) ^ llaves[idx])
+
+        self.agregar_transicion("q5", "q6", "XOR")
+
+        return texto_cifrado
+
+    def q6(self, texto_cifrado):
+        self.agregar_estado("q7", tipo="final")
+        self.agregar_transicion("q6", "q7", f"{texto_cifrado}")
 
 #######################################################
 ##################### CIFRADO RC4 #####################
@@ -66,9 +141,10 @@ def ksa(llave):
     '''Algoritmo de KSA.'''
     S = list(range(256))  # Tabla S
     key_length = len(llave)
+    llave_ascii = [ord(c) for c in llave]
     j = 0
     for i in range(256):
-        j = (j + ord(llave[i % key_length]) + S[i]) % 256
+        j = (j + llave_ascii[i % key_length] + S[i]) % 256
         S[i], S[j] = S[j], S[i]
     return S
 
@@ -76,13 +152,11 @@ def prga(S):
     '''Algoritmo de PRGA.'''
     i = 0
     j = 0
-    salida = []
-    while len():
+    while True:
         i = (i + 1) % 256
         j = (j + S[i]) % 256
         S[i], S[j] = S[j], S[i]
-        salida.append(S[(S[i] + S[j]) % 256])
-
+        yield S[(S[i] + S[j]) % 256]
 
 def rc4(llave, texto_plano, afd):
     afd.agregar_estado("q2")
@@ -93,7 +167,6 @@ def rc4(llave, texto_plano, afd):
     texto_cifrado = ""
     for char in texto_plano:
         texto_cifrado += "%02X" % (ord(char) ^ next(llaves))
-    return texto_cifrado
 
 # Función que se ejecuta al presionar el botón
 def generar_automata():
@@ -108,16 +181,19 @@ def generar_automata():
         messagebox.showerror("Error", "Por favor ingrese tanto la llave como el texto plano.")
         return
 
+
     afd = AutomataFinitoDeterminista()
-    afd.agregar_estado("q0", tipo="inicial")
-    afd.agregar_estado("q1")
-    afd.agregar_transicion("q0", "q1", f"{texto_plano}\n{llave}")
+    if afd.q0(llave):
+        if afd.q1(texto_plano, llave):
+            llave_ascii = afd.q2(llave, texto_plano)
+            S = afd.q3(llave_ascii)
+            llaves = afd.q4(S, texto_plano)
+            texto_cifrado = afd.q5(llaves, texto_plano)
+            afd.q6(texto_cifrado)
 
-    texto_cifrado = rc4(llave, texto_plano, afd)
 
-    afd.agregar_estado("q3", tipo="final")
-    afd.agregar_transicion("q2", "q3", f"{texto_cifrado}")
     afd.mostrar_automata()
+
 
 # Interfaz gráfica con Tkinter
 root = tk.Tk()
@@ -125,13 +201,13 @@ root.title("Generador de Autómata y RC4")
 root.geometry("300x200")
 
 # Etiquetas y entradas de texto
-label_llave = tk.Label(root, text="Llave:")
+label_llave = tk.Label(root, text="Llave de 5 caracteres (a-z):")
 label_llave.pack()
 
 entry_llave = tk.Entry(root)
 entry_llave.pack()
 
-label_texto = tk.Label(root, text="Texto plano:")
+label_texto = tk.Label(root, text="Texto plano de 5 a 10 carateres (a-zA-Z0-9):")
 label_texto.pack()
 
 entry_texto = tk.Entry(root)
